60edfcc80a6fa0f187deea72e716860d2b312ad8
Kostya Lyapochkin <kostya500steam@gmail.com>
2018-08-22 12:36:31 +0300

    lesson 2 a1

--- src/by/it/sandrigailo/lesson02/TaskA3.java
+++ src/by/it/sandrigailo/lesson02/TaskA3.java
@@ -7,7 +7,7 @@ package by.it.sandrigailo.lesson02;
 
 class TaskA3 {
     public static void main(String[] args) {
-        int i = 3;
+        int i = 3; 
         int j = 4;
         int k = i * i + j * j;
         System.out.println();
